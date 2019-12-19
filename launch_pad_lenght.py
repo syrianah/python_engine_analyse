58b7ad3d8d5ccd713915f26deb78220bcfa3af66
BuildTools <unconfigured@null.spigotmc.org>
2019-12-19 01:26:33 +0100

    launchpad_lenght

---
+++ launch_pad_lenght.py
@@ -0,0 +1,14 @@
+
+exit_velocity = 18
+
+rocket_velocity = 343
+rocket_mass = 3.5  # masa startowa rakiety
+
+grain_mass = 0.6  # masa paliwa
+burn_time = 1.2
+
+acceleration = (grain_mass * rocket_velocity) / (rocket_mass * burn_time)
+
+launch_pad_lenght = exit_velocity**2 / (2 * acceleration)
+
+print(launch_pad_lenght)
