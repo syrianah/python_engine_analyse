cd59613b52903d85a2ebee8d056d65ba48b398e9
syrianah <syrianahminecraft@gmail.com>
2019-12-19 13:18:33 +0100

    launch_pad

--- launch_pad_lenght.py
+++ launch_pad_lenght.py
@@ -1,10 +1,12 @@
+# http://rakiety.org.pl/forum/viewtopic.php?t=2120
 
-exit_velocity = 18
+
+exit_velocity = 13
 
 rocket_velocity = 343
-rocket_mass = 3.5  # masa startowa rakiety
+rocket_mass = 3.5       # masa startowa rakiety
 
-grain_mass = 0.6  # masa paliwa
+grain_mass = 0.6        # masa paliwa
 burn_time = 1.2
 
 acceleration = (grain_mass * rocket_velocity) / (rocket_mass * burn_time)
