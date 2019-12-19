cf9fc7951c861a5eb3a640f908ed95d872d61e96
BuildTools <unconfigured@null.spigotmc.org>
2019-12-19 01:26:52 +0100

    balistic

--- telemetry.py
+++ telemetry.py
@@ -2,16 +2,21 @@ import json
 
 thrust_sum = 0  # suma pomiarów ciągu
 mesure_num = 0  # liczba pomiarów
+burn_time = 0
 
-with open('pomiary/READOUTS_KAL_45.json') as f:
+with open('pomiary/read.json') as f:
     file = json.load(f)
     for data in file:
         thrust_sum = thrust_sum + data['thrust']
         mesure_num = mesure_num + 1
 
-mass = 0.35
+burn_time = (file[len(file)-1]['time'] - file[0]['time']) * 1000
+#print(file[0]['time'], file[len(file)-1]['time'])
+
+mass = 2
+burn_time = 2.3
 avr_thurst = (thrust_sum / mesure_num) * 10  # średni ciąg
-impuls = avr_thurst * 2.1  # impuls całkowity
+impuls = avr_thurst * burn_time  # impuls całkowity
 rocket_velocity = impuls / mass  # prędkość rakiety
 rocket_altitude = rocket_velocity**2 / (mass*9.81)  # wysokość lotu rakiety
 
@@ -19,6 +24,7 @@ print("Średni ciąg = " + str(avr_thurst))
 print("Impuls całkowity = " + str(impuls))
 print("Prędkość rakiety = " + str(rocket_velocity))
 print("Wysokość lotu rakiety = " + str(rocket_altitude))
+print("Czas spalania = " + str(burn_time))
 
 
 with open("obliczenia/test_KAL_45", "w") as f:
